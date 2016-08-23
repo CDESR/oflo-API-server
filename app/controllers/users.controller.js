616e8421a847156176d87520e2d0eb3f5ac0052f
Rencheng Chua <rench00@gmail.com>
2016-08-23 10:15:13 +0800

    allow return first name on login

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -51,7 +51,7 @@ module.exports = {
           };
           var jwt_token = jwt.sign(payload, jwt_secret, expiryObj);
 
-          return res.status(200).send({token: jwt_token, user_id: db_user.id, is_admin: db_user.admin});
+          return res.status(200).send({token: jwt_token, user_id: db_user.id, is_admin: db_user.admin, first_name: db_user.first_name});
         } else {
           return res.status(401).send({ message: 'login failed' });
         }
